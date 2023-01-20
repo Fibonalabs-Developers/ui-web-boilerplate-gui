10c6115ac8e78df813ec5be7a651a4ce4f444add
ashokkumarak1998 <105839908+ashokkumarak1998@users.noreply.github.com>
2023-01-20 12:31:54 +0530

    Added meta tags for pages (#12)
    * feat: 🎸 added meta details
    
    * fix: 🐛 commit fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,59 @@ import Commentmodule from './pages/CommentModule';
 import Utility from './pages/Utility';
 import ReactNative from './pages/ReactNative';
 import Flutter from './pages/Flutter';
+import HomePageTags from './tags/homepagetags';
+import FrontendBoilerPlateTags from './tags/Frontendboilerplatetags';
+import BackendBoilerPlateTags from './tags/BackendboilerplateTags';
+import PdfGeneratorTags from './tags/pdfgeneratortags';
+import Justcommunicationtags from './tags/justcommunicationtags';
+import Captchageneratortags from './tags/captchageneratortags';
+import CommentModuleTags from './tags/Commentmoduletags';
+import Utilitytags from './tags/utilitytags';
+import Reactnativetags from './tags/Reactnativetags';
+import FlutterTags from './tags/fluttertags';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
+          <HomePageTags />
           <HomePage />
         </Route>
         <Route exact path="/react-boilerplate">
+          <FrontendBoilerPlateTags />
           <FrontendBoilerPlate />
         </Route>
         <Route exact path="/backend-boilerplate">
+          <BackendBoilerPlateTags />
           <BackendBoilerPlate />
         </Route>
         <Route exact path="/pdf-generator">
+          <PdfGeneratorTags />
           <PdfGenerator />
         </Route>
         <Route exact path="/just-communication">
+          <Justcommunicationtags />
           <JustCommunication />
         </Route>
         <Route exact path="/captcha-generator">
+          <Captchageneratortags />
           <CaptchaGenerator />
         </Route>
         <Route exact path="/comment-module">
+          <CommentModuleTags />
           <Commentmodule />
         </Route>
         <Route exact path="/utility">
+          <Utilitytags />
           <Utility />
         </Route>
         <Route exact path="/react-native">
+          <Reactnativetags />
           <ReactNative />
         </Route>
         <Route exact path="/flutter">
+          <FlutterTags />
           <Flutter />
         </Route>
       </Switch>
---
+++ src/tags/Commentmoduletags.tsx
@@ -0,0 +1,203 @@
+import React from 'react';
+
+const CommentModuleTags = () => (
+  <>
+    <meta
+      name="description"
+      content="Comment module is the utility that can be used to achieve nested comments to help you define the approach of having comments on various posts, images, etc."
+    />
+    <meta name="keywords" content="Comment Module" />
+
+    <title>Fibonalabs: The Easiest Way to Setup Your Comment Module</title>
+    <link
+      rel="canonical"
+      href="https://opensource.fibonalabs.com/comment-module"
+    />
+
+    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-173261715-1"
+    />
+    <script>
+      {` window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-173261715-1');`}
+    </script>
+    {/* <!-- Google Tag Manager --> */}
+    <script>
+      {`
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-5DFXSBW');`}
+    </script>
+    {/* <!-- End Google Tag Manager --> */}
+    {/* {<!-- Google Tag Manager --> */}
+    <script>
+      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W268DB8');`}
+    </script>
+    {/* {<!-- End Google Tag Manager --> */}
+    {/* Organization Schema Markup Start */}
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'Organization',
+          name: 'Fibonalabs',
+          url: 'https://fibonalabs.com/',
+          logo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.cn%2Fcompany%2Ffibonalabs&psig=AOvVaw3wrS5EQf9OGMo5PdYE0b_q&ust=1628317717065000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjut6zim_ICFQAAAAAdAAAAABAZ',
+          contactPoint: {
+            '@type': 'ContactPoint',
+            telephone: '9611485544',
+            contactType: 'sales',
+            areaServed: 'IN',
+            availableLanguage: 'en',
+          },
+          sameAs: [
+            'https://www.facebook.com/fibonalabs',
+            'https://twitter.com/fibonalabs',
+            'https://www.youtube.com/channel/UCkJDB2uoWVUlGN8Jp27PBtg',
+            'https://www.linkedin.com/company/fibonalabs/',
+            'https://www.instagram.com/fibonalabs_/',
+            'https://fibonalabs.com/',
+          ],
+        }),
+      }}
+    />
+    {/* Organization Schema Markup End */}
+    {/* Local Business Schema Markup Start */}
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          '@context': 'http://schema.org',
+          '@type': 'ProfessionalService',
+          name: 'Fibonalabs',
+          image:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.cn%2Fcompany%2Ffibonalabs&psig=AOvVaw3wrS5EQf9OGMo5PdYE0b_q&ust=1628317717065000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjut6zim_ICFQAAAAAdAAAAABAZ',
+          '@id': '',
+          url: 'https://fibonalabs.com/',
+          telephone: '+1 (562) 714-7617',
+          priceRange: '50000',
+          address: {
+            '@type': 'PostalAddress',
+            streetAddress: '353 Lexington Avenue #234 Manhattan',
+            addressLocality: 'New York',
+            postalCode: '10016',
+            addressCountry: 'US',
+          },
+          openingHoursSpecification: [
+            {
+              '@type': 'OpeningHoursSpecification',
+              dayOfWeek: ['Monday'],
+              opens: '10:00 AM',
+              closes: '8:00 PM',
+            },
+            {
+              '@type': 'OpeningHoursSpecification',
+              dayOfWeek: ['Tuesday'],
+              opens: '10:00 AM',
+              closes: '8:00 PM',
+            },
+            {
+              '@type': 'OpeningHoursSpecification',
+              dayOfWeek: ['Wednesday'],
+              opens: '10:00 AM',
+              closes: '8:00 PM',
+            },
+            {
+              '@type': 'OpeningHoursSpecification',
+              dayOfWeek: ['Thursday'],
+              opens: '10:00 AM',
+              closes: '8:00 PM',
+            },
+            {
+              '@type': 'OpeningHoursSpecification',
+              dayOfWeek: ['Friday'],
+              opens: '10:00 AM',
+              closes: '8:00 PM',
+            },
+          ],
+          sameAs: [
+            'https://www.facebook.com/fibonalabs',
+            'https://twitter.com/fibonalabs',
+            'https://www.youtube.com/channel/UCkJDB2uoWVUlGN8Jp27PBtg',
+            'https://www.linkedin.com/company/fibonalabs/',
+            'https://www.instagram.com/fibonalabs_/',
+          ],
+        }),
+      }}
+    />
+
+    {/* Local Business Schema Markup End */}
+    {/*
+    <!-- Start Alexa Certify Javascript --> */}
+    <script type="text/javascript">
+      {`   _atrk_opts = { atrk_acct: "cvYzw1O7kI20L7", domain: "fibonalabs.com", dynamic: true };
+        (function () { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(as, s); })();
+  `}
+    </script>
+    <noscript>
+      {` <img
+          src="https://certify.alexametrics.com/atrk.gif?account=cvYzw1O7kI20L7"
+          style="display:none"
+          height="1"
+          width="1"
+          alt=""
+        />`}
+    </noscript>
+    {/* <!-- End Alexa Certify Javascript --> */}
+
+    {/* <!-- Open Graph protocal --> */}
+    <meta
+      property="og:title"
+      content="Digital Product Development | Agile Software Development"
+    />
+    <meta property="og:site_name" content="Fibonalabs - Product Development" />
+    <meta
+      property="og:url"
+      content="https://opensource.fibonalabs.com/comment-module"
+    />
+    <meta
+      property="og:description"
+      content="We unlock the value of your idea by building a digital product with deep knowledge of leading-edge technologies. We build high-quality digital products using an agile software development process"
+    />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:image"
+      content="https://fibonalabs.com/_next/static/image/public/images/userresearch.3f3984dc35b4bf91ab65f85083b83809.webp"
+    />
+    {/* <!-- Open Graph protocal --> */}
+    {/* <!-- Twitter card code --> */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@fibonalabs" />
+    <meta
+      name="twitter:description"
+      content="We unlock the value of your idea by building a digital product with deep knowledge of leading-edge technologies. We build high-quality digital products using an agile software development process."
+    />
+    <meta
+      name="twitter:title"
+      content="Digital Product Development | Agile Software Development"
+    />
+    <meta
+      name="twitter:image"
+      content="https://fibonalabs.com/_next/static/image/public/images/userresearch.3f3984dc35b4bf91ab65f85083b83809.webp"
+    />
+
+    {/* <!-- Twitter card code -->   */}
+  </>
+);
+
+export default CommentModuleTags;
